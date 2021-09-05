af93e2235ccaf7ae183f4a0aa587577274453864
Dilip Kumar <dilipkr7587@gmail.com>
2021-09-05 18:38:18 +0530

    fix

--- src/main/java/com/authentication/oauth/OauthApplication.java
+++ src/main/java/com/authentication/oauth/OauthApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "User API", version = "2.0", description = "User Information"))
+@OpenAPIDefinition(info = @Info(title = "User API", version = "1.0", description = "User Information"))
 @SecurityScheme(name = "oauth", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class OauthApplication {
 
